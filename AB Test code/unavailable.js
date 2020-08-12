9dcc26328790342d25b0fcfdc4e2b48bbb641aff
Jonas G <goddeerisjonas@me.com>
2020-08-12 14:27:27 +0200

    Create unavailable.js

---
+++ AB Test code/unavailable.js
@@ -0,0 +1,6 @@
+const unavailable = document.querySelector("#offlineUnavailableMsg")
+const tabs = document.querySelector("#shopCartActions > div > div.tabButtonContainer > div");
+
+if (unavailable) {
+tabs.style.display = "none";
+};
