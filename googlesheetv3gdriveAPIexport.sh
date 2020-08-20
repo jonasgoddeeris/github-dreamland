5eb26fd69dc4057e3c0a340a767fbe93af7b3cff
Jonas G <goddeerisjonas@me.com>
2020-08-20 16:13:44 +0200

    AB 25 cleanup

---
+++ googlesheetv3gdriveAPIexport.sh
@@ -0,0 +1,5 @@
+EXPORT GOOGLE SHEET AS XLSX - VIA GOOGLE DRIVE v3 API - CREATE Bearer TOKEN here https://developers.google.com/oauthplayground/
+
+
+
+curl -X GET -SSL -H "Authorization: Bearer ya29.a0AfH6SMBoxfykNbrydbQtssB2k5yNeJdbYucQ7900Nzc3x18Kv6VmOrxusY3wLqesTIQ_vGzi3vlC5ayYoQaWHKVzauYCVeMpq9gWlTlSk_okZn7pizjetZBWRkTKfUTrAfN8VjBDXTMEBug1NWG_cgKg4dVctHV_q-4"  -o "products3" "https://www.googleapis.com/drive/v3/files/14FfdrG8wU4XpebvWYeZ3K02w-Wg9Zq4S9gz60fAsS2g/export?mimeType=application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
