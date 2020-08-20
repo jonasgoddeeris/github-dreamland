7835942a2e53e9d78a4bc3942d4fd1ba8f27a2df
Jonas G <goddeerisjonas@me.com>
2020-08-20 16:15:04 +0200

    cleanup

--- AB Test code/unavailable.js
+++
@@ -1,6 +0,0 @@
-const unavailable = document.querySelector("#offlineUnavailableMsg")
-const tabs = document.querySelector("#shopCartActions > div > div.tabButtonContainer > div");
-
-if (unavailable) {
-tabs.style.display = "none";
-};
