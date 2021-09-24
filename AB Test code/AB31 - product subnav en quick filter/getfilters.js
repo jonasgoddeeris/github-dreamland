936a632733c801f01fdbf053970705cbcc313be0
Jonas G <goddeerisjonas@me.com>
2021-09-24 17:05:32 +0200

    AB66

--- AB Test code/AB31 - product subnav en quick filter/getfilters.js
+++ AB Test code/AB31 - product subnav en quick filter/getfilters.js
@@ -2,3 +2,8 @@ var specification = document.querySelector("#productsFacets");
 var firstUL = specification.getElementsByTagName('ul')[0];
 var firstFilters = firstUL.getElementsByTagName('a');
 
+for (let i = 0; i < firstFilters.length; i++) {
+  firstFilters[i].removeAttribute("role");
+}
+var filterDIV = document.getElementById('filters');
+filterDIV.innerHTML += firstFilters;
