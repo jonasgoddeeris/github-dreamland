87d7941f866b53d6c69338d571151dc102883e73
Jonas G <goddeerisjonas@me.com>
2021-11-02 13:48:09 +0100

    gdpr

---
+++ WCS/footer/gdps.js
@@ -0,0 +1,298 @@
+(function() {
+    var f = {
+        nl: {
+            privacy: {
+                link: "Privacyverklaring",
+                url: "https://mijnxtra.be/privacyverklaring",
+                text: "Door verder te gaan verklaar je de <a></a> te hebben gelezen",
+                codeToName: {
+                    x_ps_collibri: "Collibri foundation",
+                    x_ps_cssleep: "Collishop slapen",
+                    x_ps_cspullname: "Collishop Trek een naam",
+                    x_ps_xtrapresent: "Xtra Present",
+                    x_ps_colruyt_redtel: "Rode Telefoon",
+                    fr_ps_colruyt: "Colruyt Frankrijk",
+                    fr_ps_colruytretailfrance: "Colruyt Frankrijk",
+                    x_ps_cgforms: "Colruyt Group",
+                    x_ps_cgjobs: "Colruyt Group jobs",
+                    x_ps_boniweb: "Boni Selection",
+                    x_ps_dreambaby_infonight: "Dreambaby infoavond",
+                    x_ps_dbnewstory: "NewStory",
+                    x_ps_dlevents: "Dreamland events",
+                    x_ps_dlvoucher: "Dreamland vouchers",
+                    x_ps_dlfoto: "Dreamland foto&#39;s",
+                    fr_ps_dreamland: "Dreamland",
+                    x_ps_eolycons: "Eoly Particulieren",
+                    x_ps_eolycoop: "Eoly Co&#246;peratie",
+                    x_ps_soluciouscons: "Solucious B2C",
+                    x_ps_sparcontact: "Spar contact",
+                    x_ps_dlwishlist: "Dreamland verlanglijstjes",
+                    x_ps_xtra: "Xtra",
+                    x_ps_dllootcadeau: "Dreamland loot-je-cadeautje",
+                    x_ps_xtraneighbourhood: "Xtra Buurt",
+                    x_ps_datsenergygas: "particulieren",
+                    x_ps_incontactforms: "contactformulier",
+                    x_ps_collectgohomedelivery: "Collect&amp;Go aanhuislevering",
+                    x_ps_dreamland_sint: "Dreamland Sint"
+                }
+            },
+            terms: {
+                link: "Algemene voorwaarden",
+                url: "https://mijnxtra.be/algemene-voorwaarden",
+                text: "Ik ga akkoord met de <a></a>",
+                codeToName: {
+                    x_tc_mycolruyt: "MyColruyt app",
+                    x_tc_clpbaby: "Colruyt Baby aan boord",
+                    x_tc_clpcohabitate: "Colruyt Samenwoonactie&#160;",
+                    x_tc_clptombola: "Colruyt Tombola",
+                    x_tc_clptuttifrutti: "Tutti Frutti",
+                    x_tc_clpcamp: "Colruyt op kamp",
+                    x_tc_clpwinespirit: "Colruyt klassewijnen en sterke dranken&#160;",
+                    x_tc_clpbh: "Colruyt Beenhouwerij",
+                    fr_tc_colplus: "Colruyt Frankrijk",
+                    x_tc_collectgo: "Collect &amp; Go",
+                    fr_tc_collectgo: "Collect &amp; Go",
+                    x_tc_collishop_web: "Collishop webshop",
+                    x_tc_xtrapresent: "Xtra Present",
+                    x_tc_cgfoodtasting: "Smaaktesten",
+                    x_tc_cga: "Colruyt Group Academy",
+                    x_tc_cru: "CRU",
+                    x_tc_dats24loadb2c: "DATS 24-laadpas",
+                    x_tc_dreambaby_web: "Dreambaby webshop",
+                    x_tc_dbgiftlist: "Dreambaby geboortelijst",
+                    x_tc_dbnewstoryseller: "Dreambaby Newstory verkoper",
+                    x_tc_dbnewstorybuyer: "Dreambaby Newstory koper",
+                    x_tc_dreamland_web: "Dreamland webshop",
+                    x_tc_dlvoucher: "Dreamland Vouchers&#160;",
+                    x_tc_dlevents: "Dreamland Events",
+                    x_tc_scanpaygo: "&#45;Scan&#46;Pay&#46;Go",
+                    x_tc_xtra: "Xtra",
+                    x_tc_xtrapay: "Xtra Betaalfunctie",
+                    x_tc_xtrapresentaccount: "Xtra Present Account",
+                    x_tc_xtrapresentplatform: "Xtra Present Platform",
+                    x_tc_xtraneighbourhood: "Xtra Buurt",
+                    x_tc_datsenergygas: "energie particulieren",
+                    x_tc_collectgohomedelivery: "Collect&amp;Go aanhuislevering",
+                    x_tc_dreamland_sint: "Dreamland Sint"
+                }
+            }
+        },
+        fr: {
+            privacy: {
+                link: "D&eacute;claration de confidentialit&eacute;",
+                url: "https://monxtra.be/declaration-de-confidentialite",
+                text: "En continuant, vous marquez votre accord avec la <a></a>",
+                codeToName: {
+                    x_ps_collibri: "Collibri foundation",
+                    x_ps_cssleep: "Collishop dormir",
+                    x_ps_cspullname: "Collishop&#160;Tire un nom",
+                    x_ps_xtrapresent: "Xtra Present",
+                    x_ps_colruyt_redtel: "T&#233;l&#233;phone Rouge",
+                    fr_ps_colruyt: "Colruyt France",
+                    fr_ps_colruytretailfrance: "Colruyt France",
+                    x_ps_cgforms: "Colruyt Group",
+                    x_ps_cgjobs: "Colruyt Group Jobs",
+                    x_ps_boniweb: "Boni Selection",
+                    x_ps_dreambaby_infonight: "Dreambaby soir&#233;e d&#39;information",
+                    x_ps_dbnewstory: "NewStory",
+                    x_ps_dlevents: "Dreamland events",
+                    x_ps_dlvoucher: "Dreamland voucher",
+                    x_ps_dlfoto: "Dreamland photos",
+                    fr_ps_dreamland: "Dreamland",
+                    x_ps_eolycons: "Eoly Particuliers",
+                    x_ps_eolycoop: "Eoly Coop&#233;ration",
+                    x_ps_soluciouscons: "Solucious B2C",
+                    x_ps_sparcontact: "Spar contact",
+                    x_ps_dlwishlist: "Dreamland listes de souhaits",
+                    x_ps_xtra: "Xtra",
+                    x_ps_dllootcadeau: "Dreamland qui-offre-a-qui",
+                    x_ps_xtraneighbourhood: "Xtra Quartier",
+                    x_ps_datsenergygas: "particuliers",
+                    x_ps_incontactforms: "formulaire de contact",
+                    x_ps_collectgohomedelivery: "Collect&amp;Go livraison &agrave; domicile",
+                    x_ps_dreamland_sint: "Dreamland saint Nicolas"
+                }
+            },
+            terms: {
+                link: "Conditions g&eacute;n&eacute;rales",
+                url: "https://monxtra.be/conditions-generales",
+                text: "Je suis d'accord avec les <a></a>",
+                codeToName: {
+                    x_tc_mycolruyt: "MyColruyt app",
+                    x_tc_clpbaby: "Colruyt B&#233;b&#233; &#224; bord",
+                    x_tc_clpcohabitate: "Colruyt Vivre ensemble",
+                    x_tc_clptombola: "Colruyt Tombola",
+                    x_tc_clptuttifrutti: "Tutti Frutti",
+                    x_tc_clpcamp: "Colruyt au camp",
+                    x_tc_clpwinespirit: "Colruyt Grands vins et spiritueux",
+                    x_tc_clpbh: "Colruyt Boucherie",
+                    fr_tc_colplus: "Colruyt France",
+                    x_tc_collectgo: "Collect &amp; Go",
+                    fr_tc_collectgo: "Collect &amp; Go",
+                    x_tc_collishop_web: "Collishop webshop",
+                    x_tc_xtrapresent: "Xtra Present",
+                    x_tc_cgfoodtasting: "Tests de go&#251;t",
+                    x_tc_cga: "Colruyt Group Academy",
+                    x_tc_cru: "CRU",
+                    x_tc_dats24loadb2c: "DATS 24 Carte de recharge",
+                    x_tc_dreambaby_web: "Dreambaby webshop",
+                    x_tc_dbgiftlist: "Dreambaby&#160;liste de naissance",
+                    x_tc_dbnewstoryseller: "Dreambaby Newstory vendeur",
+                    x_tc_dbnewstorybuyer: "Dreambaby Newstory acheteur",
+                    x_tc_dreamland_web: "Dreamland webshop",
+                    x_tc_dlvoucher: "Dreamland Voucher",
+                    x_tc_dlevents: "Dreamland Events",
+                    x_tc_scanpaygo: "&#45;Scan&#46;Pay&#46;Go",
+                    x_tc_xtra: "Xtra",
+                    x_tc_xtrapay: "Xtra fonction de paiement",
+                    x_tc_xtrapresentaccount: "Xtra Present Account",
+                    x_tc_xtrapresentplatform: "Xtra Present Platform",
+                    x_tc_xtraneighbourhood: "Xtra Quartier",
+                    x_tc_datsenergygas: "&eacute;nergie particuliers",
+                    x_tc_collectgohomedelivery: "Collect&amp;Go livraison &agrave; domicile",
+                    x_tc_dreamland_sint: "Dreamland saint Nicolas"
+                }
+            }
+        },
+        en: {
+            privacy: {
+                link: "Privacystatement",
+                url: "https://monxtra.be/declaration-de-confidentialite",
+                text: "By proceeding, you acknowledge that you have read the <a></a>",
+                codeToName: {
+                    x_ps_cgforms: "Colruyt Group"
+                }
+            },
+            terms: {
+                link: "Terms and conditions",
+                url: "https://monxtra.be/conditions-generales",
+                text: "I accept the <a></a>",
+                codeToName: {}
+            }
+        },
+        de: {
+            privacy: {
+                link: "Datenschutzerkl&auml;rung",
+                url: "https://monxtra.be/declaration-de-confidentialite",
+                text: "Indem wir fortfahren, das Sie diese <a></a> gelesen haben",
+                codeToName: {
+                    x_ps_xtra: "XTRA"
+                }
+            },
+            terms: {
+                link: "Allgemeinen Bedingungen",
+                url: "https://monxtra.be/conditions-generales",
+                text: "Ich akzeptiere die <a></a>",
+                codeToName: {}
+            }
+        }
+    }
+      , m = {
+        privacy: "x_ps_xtra",
+        terms: "x_tc_xtra"
+    }
+      , h = function(a) {
+        return a ? a.toLowerCase() : a
+    }
+      , n = function(a, b, c) {
+        a = Object.create(a.prototype);
+        a.constructor = b;
+        for (var d in c)
+            c.hasOwnProperty(d) && (a[d] = c[d]);
+        return a
+    }
+      , g = function(a, b) {
+        var c = this._determineLang(a)
+          , d = h(a.getAttribute("code")) || m[b]
+          , e = {
+            tagName: b,
+            lang: c,
+            display: this._determineDisplay(a),
+            code: d,
+            name: f[c || "nl"][b].codeToName[d],
+            prefix: a.getAttribute("prefix") || "",
+            suffix: a.getAttribute("suffix") || ""
+        };
+        c = "";
+        e.lang || (c += "<br>'" + b + "' tag requires tag to have a 'lang' attribute with value 'nl', 'fr', 'en' or 'de' !");
+        e.display || (c += "<br>'" + b + "' tag requires tag to have a 'display' attribute with value 'link' or 'accept' !");
+        e.name || "de" !== e.lang && "en" !== e.lang || (console.info("'" + b + "' tag got unknown 'code' attribute value '" + e.code + "' for language '" + e.lang + "', so falling back to 'fr' as language..."),
+        e.lang = "fr",
+        e.name = f.fr[b].codeToName[d]);
+        e.code && !e.name && (c += "<br>'" + b + "' tag got unknown 'code' attribute value '" + e.code + "', ask Xtra team to add it if needed !");
+        c ? (d = document.createElement("span"),
+        d.innerHTML = c,
+        a.parentNode.replaceChild(d, a)) : a.parentNode.replaceChild(this._buildNode(e), a)
+    };
+    g.prototype = {
+        _buildNode: function(a) {
+            if ("link" === a.display) {
+                var b = this._initLink(a, document.createElement("a"));
+                b.className = "gdpr_link_" + a.tagName
+            } else
+                b = document.createElement("span"),
+                b.className = "gdpr_accept_" + a.tagName,
+                b.innerHTML = f[a.lang][a.tagName].text,
+                this._initLink(a, b.querySelector("a"), !0);
+            return b
+        },
+        _initLink: function(a, b, c) {
+            var d = f[a.lang][a.tagName]
+              , e = a.prefix;
+            c = c ? (c = d.link) ? h(c.substr(0, 1)) + c.substr(1) : c : d.link;
+            e = e + c + (" " + a.name);
+            e += a.suffix;
+            b.innerHTML = e;
+            d = d.url;
+            this.completeUrl && (d = this.completeUrl(d, a));
+            b.href = this._correctEnvironment(d);
+            b.target = "_blank";
+            return b
+        },
+        _determineLang: function(a) {
+            if ((a = h(a.getAttribute("lang"))) && f[a])
+                return a
+        },
+        _determineDisplay: function(a) {
+            if ((a = h(a.getAttribute("display"))) && ("link" === a || "accept" === a))
+                return a
+        },
+        _correctEnvironment: function(a) {
+            var b = window.location.host;
+            -1 !== b.indexOf("xtra.dev.") ? (a = a.replace("https://mijnxtra.be", "http://mijnxtra.dev.colruytgroup.com:9080/xtra/nl"),
+            a = a.replace("https://monxtra.be", "http://monxtra.dev.colruytgroup.com:9080/xtra/fr")) : -1 !== b.indexOf("localhost") || -1 !== b.indexOf(".test.") ? (a = a.replace("https://mijnxtra.be", "https://mijnxtra.test.colruytgroup.com"),
+            a = a.replace("https://monxtra.be", "https://monxtra.test.colruytgroup.com")) : -1 !== b.indexOf(".syst.") && (a = a.replace("https://mijnxtra.be", "https://mijnxtra.syst.colruytgroup.com"),
+            a = a.replace("https://monxtra.be", "https://monxtra.syst.colruytgroup.com"));
+            return a
+        }
+    };
+    var k = function(a, b) {
+        g.call(this, a, b)
+    };
+    k.prototype = n(g, k, {
+        completeUrl: function(a, b) {
+            "en" === b.lang || "de" === b.lang ? a += "/__" + b.lang + "__" + b.code : m.privacy !== b.code && (a += "/" + b.code);
+            return a
+        }
+    });
+    var l = function(a, b) {
+        g.call(this, a, b)
+    };
+    l.prototype = n(g, l, {
+        completeUrl: function(a, b) {
+            return a = "en" === b.lang || "de" === b.lang ? a + ("/__" + b.lang + "__" + b.code) : a + ("/" + b.code)
+        }
+    });
+    var p = function(a, b) {
+        for (var c = document.getElementsByTagName(a), d = c.length - 1; d < c.length && 0 <= d; d--)
+            new b(c[d],a)
+    };
+    window.GdprTags = window.GdprTags || {};
+    window.GdprTags.render = function() {
+        p("privacy", k);
+        p("terms", l)
+    }
+    ;
+    window.GdprTags.render()
+}
+)();
